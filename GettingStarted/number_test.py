5d92ba398280c09d3cec169d87e618d1c8472071
carlinhocr <carlos.rienzi@gmail.com>
2024-12-06 16:39:07 -0300

    Chapter 2

---
+++ GettingStarted/number_test.py
@@ -0,0 +1,7 @@
+number_test = input("Enter a Number:")
+if int(number_test) > 5:
+    print ('the number is higher than 5!!')
+elif int(number_test) < 5:
+    print ('the number is smaller than 5!!')
+else:
+    print ('the number is  5!!')
