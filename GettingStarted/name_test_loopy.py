3f30a646f1e541a29bd54d99fb15946766e87aa0
carlinhocr <carlos.rienzi@gmail.com>
2024-12-06 16:34:44 -0300

    Chapter 2

---
+++ GettingStarted/name_test_loopy.py
@@ -0,0 +1,5 @@
+user_name = input("what's your name?:")
+while user_name != "Clark Kent":
+    print ('You are not Superman !!, try again')
+    user_name = input("what's your name?:")
+print ('You are Superman !!')
